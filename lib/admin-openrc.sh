c14fc91aa6fda3e561b63a0e5381130adfd18c2a
laurencechan <laurencechan@qq.com>
2018-02-07 08:53:24 +0800

    Initial commit

---
+++ lib/admin-openrc.sh
@@ -0,0 +1,5 @@
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_IDENTITY_API_VERSION=3
---
+++ lib/demo-openrc.sh
@@ -0,0 +1,5 @@
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_IDENTITY_API_VERSION=3
